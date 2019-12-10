b8f59a956b8d78b358a027971107914f5a168be4
abubaca4 <abubaca4@gmail.com>
2019-12-10 19:49:37 +0300

    Dijkstra fix

--- Dijkstra/Source.cpp
+++ Dijkstra/Source.cpp
@@ -7,11 +7,11 @@ using namespace std;
 int main()
 {
 	size_t n;
-	int i;
+	int start;
 	cout << "Enter count of vertex:";
 	cin >> n;
 	cout << "Enter start vertex:";
-	cin >> i;
+	cin >> start;
 	Graph<pair<int, size_t>, int> data(n);
 	cout << "Enter matrix:" << endl;
 	for (size_t i=0; i< n; i++)
@@ -21,27 +21,29 @@ int main()
 			if (data.edge(i, j) != -1)
 				data.add_edge(i, j);
 		}
-	data.vertex(0) = make_pair(1, 0);
-	for (size_t i = 1; i < n; i++)
+	data.vertex(start) = make_pair(1, 0);
+	for (size_t i = 0; i < start; i++)
 		data.vertex(i) = make_pair(1, (size_t)1e9);
-	while (i != -1)
+	for (size_t i = start + 1; i < n; i++)
+		data.vertex(i) = make_pair(1, (size_t)1e9);
+	while (start != -1)
 	{
-		for (auto it = data.vertex_n(i); it != data.end();++it)
+		for (auto it = data.vertex_n(start); it != data.end();++it)
 		{
-			if (data.vertex(*it).second > data.vertex(i).second + data.edge(i, *it))
+			if (data.vertex(*it).second > data.vertex(start).second + data.edge(start, *it))
 			{
-				data.vertex(*it).second = data.vertex(i).second + data.edge(i, *it);
+				data.vertex(*it).second = data.vertex(start).second + data.edge(start, *it);
 				data.vertex(*it).first = 0;
 			}
 		}
-		data.vertex(i).first = -1;
+		data.vertex(start).first = -1;
 		int min = -1;
-		for (size_t j=0; j<n; j++)
-			if (data.vertex(j).first == 0 && (min == -1 || data.vertex(j).second < data.vertex(min).second))
-				min = j;
-		i = min;
+		for (size_t i=0; i<n; i++)
+			if (data.vertex(i).first == 0 && (min == -1 || data.vertex(i).second < data.vertex(min).second))
+				min = i;
+		start = min;
 	}
 	cout << "Distances to vertexs:" << endl;
-	for (size_t j = 0; j < n; j++)
-		cout << data.vertex(j).second << " ";
+	for (size_t i = 0; i < n; i++)
+		cout << data.vertex(i).second << " ";
 }
